e9c798fe234e32fcfcca79c71ef9b2c9bd5cc486
Grace Skinner <graski@ceh.ac.uk>
2019-02-14 10:32:40 +0000

    updating additional ON OFFs, adding copy of rclocal contents

--- scripts/wpi_script_generator_ags.py
+++ scripts/wpi_script_generator_ags.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
     end_time = datetime(next_sunday_dt.year, next_sunday_dt.month, next_sunday_dt.day, 12, 20)
     
     # Add additional ON-OFF before the recording schedule starts
-    # 0- ON from 1 Jan 2000 
-    on_from_0 = datetime(2000, 1, 1, 0, 0, 0)
+    # 0- ON from 1 Jan 2010 
+    on_from_0 = datetime(2010, 5, 1, 0, 0, 0)
     on_to_0 = start_time - timedelta(minutes=5)
     on_duration_days_0, on_duration_hours_0, on_duration_minutes_0, on_duration_seconds_0 = time_difference(on_from_0, on_to_0)
     
@@ -136,14 +136,14 @@ if __name__ == "__main__":
     off_duration_days_7, off_duration_hours_7, off_duration_minutes_7, off_duration_seconds_7 = time_difference(off_from_7, off_to_7)
     
     # Add additional ON-OFF after the recording schedule ends
-    # 8- ON from Sunday 12:20 to 31 Dec 2029
+    # 8- ON from Sunday 12:20 to 31 Dec 2097
     on_from_8 = off_to_7
-    on_to_8 = datetime(2029, 12, 31, 23, 59, 59)
+    on_to_8 = datetime(2037, 12, 31, 23, 59, 59)
     on_duration_days_8, on_duration_hours_8, on_duration_minutes_8, on_duration_seconds_8 = time_difference(on_from_8, on_to_8)
     
-    # 8- OFF from 31 Dec 2029 to 1 Jan 2030
+    # 8- OFF from 31 Dec 2097 to 1 Jan 2098
     off_from_8 = on_to_8
-    off_to_8 = datetime(2030, 1, 1, 1, 0, 0) 
+    off_to_8 = datetime(2038, 1, 1, 1, 0, 0) 
     off_duration_days_8, off_duration_hours_8, off_duration_minutes_8, off_duration_seconds_8 = time_difference(off_from_8, off_to_8)
 
     # Generate Witty Pi schedule
