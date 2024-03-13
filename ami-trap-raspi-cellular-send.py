36550f7cf9c67d64b73573f01e03505b0d904a4f
Jonas Beuchert <jonbeu@ceh.ac.uk>
2024-03-13 12:05:06 +0000

    Add scripts to directly send data or send & receive data
    Addressing issue https://github.com/AMI-system/ami_setup/issues/12

---
+++ ami-trap-raspi-cellular-send.py
@@ -0,0 +1,7 @@
+from amitrap_cellular import cellular_send
+import asyncio
+
+
+if __name__ == "__main__":
+
+    asyncio.run(cellular_send())
---
+++ ami-trap-raspi-cellular-send_and_receive.py
@@ -0,0 +1,7 @@
+from amitrap_cellular import cellular_send_and_receive
+import asyncio
+
+
+if __name__ == "__main__":
+
+    asyncio.run(cellular_send_and_receive())
