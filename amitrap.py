664e6b187d1c5253d8177324f84ae60532cfc2b1
Jonas Beuchert <jonbeu@ceh.ac.uk>
2024-04-24 15:47:14 +0100

    Increase timeout

--- amitrap.py
+++ amitrap.py
@@ -332,7 +332,7 @@ class AmiTrap:
                 bash_cmd = f"printf '1\n13\n' | {self.wittypi_path}/wittyPi.sh"
                 print(bash_cmd)
                 print()
-                subprocess.run(bash_cmd, check=True, timeout=2)
+                subprocess.run(bash_cmd, check=True, timeout=5)
             except Exception as e:
                 print("Could not set RTC time. Is there an issue with the WittyPi?")
                 print()
