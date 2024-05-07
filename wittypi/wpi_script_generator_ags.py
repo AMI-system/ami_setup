8b785baf3787162189bc6ff82acc52a3bfe559df
Alba Gomez Segura <gomezsegura.alba@gmail.com>
2024-05-07 21:49:36 +0300

    Fix wittypi schedule generator to start with ON and finish with OFF times

--- wittypi/wpi_script_generator_ags.py
+++ wittypi/wpi_script_generator_ags.py
@@ -39,6 +39,21 @@ def read_json_config(json_path):
         return json.load(json_file)
 
 
+def get_previous_sunday():
+    # Get today's date
+    today = datetime.now()
+    
+    # Get the day of the week (0 = Monday, 6 = Sunday)
+    day_of_week = today.weekday()
+    
+    if day_of_week == 6:  # If today is Sunday
+        return today
+    else:
+        # Calculate the date for the previous Sunday
+        previous_sunday = today - timedelta(days=day_of_week + 1)
+        return previous_sunday
+
+
 # Time differecne between two dates
 def time_difference(start_date, end_date):
     time_delta = end_date - start_date
@@ -46,11 +61,12 @@ def time_difference(start_date, end_date):
     # Get the total seconds in the time difference
     total_seconds = time_delta.total_seconds()
 
-    # Convert time delta to hours and minutes
-    hours = int(total_seconds) // 3600
-    minutes = int((total_seconds % 3600) // 60)
+    # Convert time delta to days, hours, and minutes
+    days = int(total_seconds) // (24 * 3600)
+    hours = (int(total_seconds) % (24 * 3600)) // 3600
+    minutes = int(total_seconds % 3600) // 60
 
-    return hours, minutes
+    return days, hours, minutes
 
 
 # Replace 'config.json' with the actual path to your JSON configuration file
@@ -63,7 +79,8 @@ json_config = read_json_config(json_config_path)
 device_settings = json_config['device_settings']
 
 # Find today's and tomorrow's dates
-sunday_dt = datetime.now()
+sunday_dt = get_previous_sunday()
+print("last Sunday date:", sunday_dt)
 monday_dt = sunday_dt + timedelta(1)
 tuesday_dt = sunday_dt + timedelta(2)
 wednesday_dt = sunday_dt + timedelta(3)
@@ -83,67 +100,67 @@ _, saturday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_se
 # Every schedule starts on a Monday and ends on a Sunday, total Witty Pi schedule to cover this full week schedule
 # Overlapping schedule starts 10 minutes earlier to the nearest schedule and ends 10 minutes later to the last schedule
 start_time = monday_sunset - timedelta(minutes=10)
-end_time = datetime(next_sunday_dt.year, next_sunday_dt.month, next_sunday_dt.day, 12, 10)
+end_time = datetime(next_sunday_dt.year, next_sunday_dt.month, next_sunday_dt.day, 12, 20)
 
-# 1- OFF from Sunday at 12:10 to Monday at sunset-10min
-off_from_1 = datetime(sunday_dt.year, sunday_dt.month, sunday_dt.day, 12, 10)
-off_to_1 = monday_sunset - timedelta(minutes=10)
-off_duration_hours_1, off_duration_minutes_1 = time_difference(off_from_1, off_to_1)
+# 1- ON from Monday sunset-10min to Tuesday sunrise+10min
+on_from_1 = start_time
+on_to_1 = tuesday_sunrise + timedelta(minutes=10)
+on_duration_days_1, on_duration_hours_1, on_duration_minutes_1 = time_difference(on_from_1, on_to_1)
 
-# 2- ON from Monday sunset-10min to Tuesday sunrise+10min
-on_from_2 = off_to_1
-on_to_2 = tuesday_sunrise + timedelta(minutes=10)
-on_duration_hours_2, on_duration_minutes_2 = time_difference(on_from_2, on_to_2)
+# 2- OFF from Tuesday sunrise+10min to Tuesday 11:50
+off_from_2 = on_to_1
+off_to_2 = datetime(tuesday_dt.year, tuesday_dt.month, tuesday_dt.day, 11, 50)
+off_duration_days_2, off_duration_hours_2, off_duration_minutes_2 = time_difference(off_from_2, off_to_2)
 
-# 3- OFF from Tuesday sunrise+10min to Tuesday 11:50
-off_from_3 = on_to_2
-off_to_3 = datetime(tuesday_dt.year, tuesday_dt.month, tuesday_dt.day, 11, 50)
-off_duration_hours_3, off_duration_minutes_3 = time_difference(off_from_3, off_to_3)
+# 3- ON from Tuesday 11:50 to Wednesday 12:10
+on_from_3 = off_to_2
+on_to_3 = datetime(wednesday_dt.year, wednesday_dt.month, wednesday_dt.day, 12, 10)
+on_duration_days_3, on_duration_hours_3, on_duration_minutes_3 = time_difference(on_from_3, on_to_3)
 
-# 4- ON from Tuesday 11:50 to Wednesday 12:10
-on_from_4 = off_to_3
-on_to_4 = datetime(wednesday_dt.year, wednesday_dt.month, wednesday_dt.day, 12, 10)
-on_duration_hours_4, on_duration_minutes_4 = time_difference(on_from_4, on_to_4)
+# 4- OFF from Wednesday 12:10 to Wednesday sunset-10min
+off_from_4 = on_to_3
+off_to_4 = wednesday_sunset - timedelta(minutes=10)
+off_duration_days_4, off_duration_hours_4, off_duration_minutes_4 = time_difference(off_from_4, off_to_4)
 
-# 5- OFF from Wednesday 12:10 to Wednesday sunset-10min
-off_from_5 = on_to_4
-off_to_5 = wednesday_sunset - timedelta(minutes=10)
-off_duration_hours_5, off_duration_minutes_5 = time_difference(off_from_5, off_to_5)
+# 5- ON from Wednesday sunset-10min to Thursday sunrise+10min
+on_from_5 = off_to_4
+on_to_5 = thursday_sunrise + timedelta(minutes=10)
+on_duration_days_5, on_duration_hours_5, on_duration_minutes_5 = time_difference(on_from_5, on_to_5)
 
-# 6- ON from Wednesday sunset-10min to Thursday sunrise+10min
-on_from_6 = off_to_5
-on_to_6 = thursday_sunrise + timedelta(minutes=10)
-on_duration_hours_6, on_duration_minutes_6 = time_difference(on_from_6, on_to_6)
+# 6- OFF from Thursday sunrise+10min to Thursday 11:50
+off_from_6 = on_to_5
+off_to_6 = datetime(thursday_dt.year, thursday_dt.month, thursday_dt.day, 11, 50)
+off_duration_days_6, off_duration_hours_6, off_duration_minutes_6 = time_difference(off_from_6, off_to_6)
 
-# 7- OFF from Thursday sunrise+10min to Thursday 11:50
-off_from_7 = on_to_6
-off_to_7 = datetime(thursday_dt.year, thursday_dt.month, thursday_dt.day, 11, 50)
-off_duration_hours_7, off_duration_minutes_7 = time_difference(off_from_7, off_to_7)
+# 7- ON from Thursday 11:50 to Friday 12:10
+on_from_7 = off_to_6
+on_to_7 = datetime(friday_dt.year, friday_dt.month, friday_dt.day, 12, 10)
+on_duration_days_7, on_duration_hours_7, on_duration_minutes_7 = time_difference(on_from_7, on_to_7)
 
-# 8- ON from Thursday 11:50 to Friday 12:10
-on_from_8 = off_to_7
-on_to_8 = datetime(friday_dt.year, friday_dt.month, friday_dt.day, 12, 10)
-on_duration_hours_8, on_duration_minutes_8 = time_difference(on_from_8, on_to_8)
+# 8- OFF from Friday 12:10 to Friday sunset-10min
+off_from_8 = on_to_7
+off_to_8 = friday_sunset - timedelta(minutes=10)
+off_duration_days_8, off_duration_hours_8, off_duration_minutes_8 = time_difference(off_from_8, off_to_8)
 
-# 9- OFF from Friday 12:10 to Friday sunset-10min
-off_from_9 = on_to_8
-off_to_9 = friday_sunset - timedelta(minutes=10)
-off_duration_hours_9, off_duration_minutes_9 = time_difference(off_from_9, off_to_9)
+# 9- ON from Friday sunset-10min to Saturday sunrise+10min
+on_from_9 = off_to_8
+on_to_9 = saturday_sunrise + timedelta(minutes=10)
+on_duration_days_9, on_duration_hours_9, on_duration_minutes_9 = time_difference(on_from_9, on_to_9)
 
-# 10- ON from Friday sunset-10min to Saturday sunrise+10min
-on_from_10 = off_to_9
-on_to_10 = saturday_sunrise + timedelta(minutes=10)
-on_duration_hours_10, on_duration_minutes_10 = time_difference(on_from_10, on_to_10)
+# 10- OFF from Saturday sunrise+10min to Saturday 11:50
+off_from_10 = on_to_9
+off_to_10 = datetime(saturday_dt.year, saturday_dt.month, saturday_dt.day, 11, 50)
+off_duration_days_10, off_duration_hours_10, off_duration_minutes_10 = time_difference(off_from_10, off_to_10)
 
-# 11- OFF from Saturday sunrise+10min to Saturday 11:50
-off_from_11 = on_to_10
-off_to_11 = datetime(saturday_dt.year, saturday_dt.month, saturday_dt.day, 11, 50)
-off_duration_hours_11, off_duration_minutes_11 = time_difference(off_from_11, off_to_11)
+# 11- ON from Saturday 11:50 to Sunday 12:10
+on_from_11 = off_to_10
+on_to_11 = datetime(next_sunday_dt.year, next_sunday_dt.month, next_sunday_dt.day, 12, 10)
+on_duration_days_11, on_duration_hours_11, on_duration_minutes_11 = time_difference(on_from_11, on_to_11)
 
-# 12- ON from Saturday 11:50 to Sunday 12:10
-on_from_12 = off_to_11
-on_to_12 = datetime(next_sunday_dt.year, next_sunday_dt.month, next_sunday_dt.day, 12, 10)
-on_duration_hours_12, on_duration_minutes_12 = time_difference(on_from_12, on_to_12)
+# 12- OFF from Sunday 12:10 to Sunday 12:20
+off_from_12 = on_to_11
+off_to_12 = datetime(next_sunday_dt.year, next_sunday_dt.month, next_sunday_dt.day, 12, 20)
+off_duration_days_12, off_duration_hours_12, off_duration_minutes_12 = time_difference(off_from_12, off_to_12)
 
 
 # Generate Witty Pi schedule
@@ -151,21 +168,21 @@ on_duration_hours_12, on_duration_minutes_12 = time_difference(on_from_12, on_to
 witty_pi_schedule = f"""
 BEGIN {start_time}
 END {end_time}
-OFF H{off_duration_hours_1} M{off_duration_minutes_1}
-ON H{on_duration_hours_2} M{on_duration_minutes_2}
-OFF H{off_duration_hours_3} M{off_duration_minutes_3}
-ON H{on_duration_hours_4} M{on_duration_minutes_4}
-OFF H{off_duration_hours_5} M{off_duration_minutes_5}
-ON H{on_duration_hours_6} M{on_duration_minutes_6}
-OFF H{off_duration_hours_7} M{off_duration_minutes_7}
-ON H{on_duration_hours_8} M{on_duration_minutes_8}
-OFF H{off_duration_hours_9} M{off_duration_minutes_9}
-ON H{on_duration_hours_10} M{on_duration_minutes_10}
-OFF H{off_duration_hours_11} M{off_duration_minutes_11}
-ON H{on_duration_hours_12} M{on_duration_minutes_12}
+ON D{on_duration_days_1} H{on_duration_hours_1} M{on_duration_minutes_1}
+OFF D{off_duration_days_2} H{off_duration_hours_2} M{off_duration_minutes_2}
+ON D{on_duration_days_3} H{on_duration_hours_3} M{on_duration_minutes_3}
+OFF D{off_duration_days_4} H{off_duration_hours_4} M{off_duration_minutes_4}
+ON D{on_duration_days_5} H{on_duration_hours_5} M{on_duration_minutes_5}
+OFF D{off_duration_days_6} H{off_duration_hours_6} M{off_duration_minutes_6}
+ON D{on_duration_days_7} H{on_duration_hours_7} M{on_duration_minutes_7}
+OFF D{off_duration_days_8} H{off_duration_hours_8} M{off_duration_minutes_8}
+ON D{on_duration_days_9} H{on_duration_hours_9} M{on_duration_minutes_9}
+OFF D{off_duration_days_10} H{off_duration_hours_10} M{off_duration_minutes_10}
+ON D{on_duration_days_11} H{on_duration_hours_11} M{on_duration_minutes_11}
+OFF D{off_duration_days_12} H{off_duration_hours_12} M{off_duration_minutes_12}
 """
 
-print(witty_pi_schedule)
+# print(witty_pi_schedule)
 
 # Specify the target path for the schedule file
 target_path = '/home/pi/scripts/'
@@ -176,7 +193,7 @@ schedule_file_path = f'{target_path}witty_pi_schedule.wpi'
 with open(schedule_file_path, 'w') as file:
     file.write(witty_pi_schedule)
 
-print(f"Witty Pi schedule generated and saved to {schedule_file_path}.")
+# print(f"Witty Pi schedule generated and saved to {schedule_file_path}.")
 
 # Move the schedule file to the target path
 #shutil.move('schedule.wpi', schedule_file_path)
