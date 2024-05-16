480bdb5c8260bc184984bfde631b347dc9b7f70f
Alba Gomez Segura <gomezsegura.alba@gmail.com>
2024-05-16 11:17:36 +0100

    Fix timezone error.

--- wittypi/wpi_script_generator_ags.py
+++ wittypi/wpi_script_generator_ags.py
@@ -31,12 +31,23 @@ if __name__ == "__main__":
     next_sunday_dt = sunday_dt + timedelta(7)
 
     # Get sunset and sunrise for each day
-    monday_sunset, _ = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], monday_dt).replace(tzinfo=None)
-    _, tuesday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], tuesday_dt).replace(tzinfo=None)
-    wednesday_sunset, _ = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], wednesday_dt).replace(tzinfo=None)
-    _, thursday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], thursday_dt).replace(tzinfo=None)
-    friday_sunset, _ = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], friday_dt).replace(tzinfo=None)
-    _, saturday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], saturday_dt).replace(tzinfo=None)
+    monday_sunset, _ = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], monday_dt)
+    monday_sunset = monday_sunset.replace(tzinfo=None)
+
+    _, tuesday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], tuesday_dt)
+    tuesday_sunrise = tuesday_sunrise.replace(tzinfo=None)
+
+    wednesday_sunset, _ = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], wednesday_dt)
+    wednesday_sunset = wednesday_sunset.replace(tzinfo=None)
+
+    _, thursday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], thursday_dt)
+    thursday_sunrise = thursday_sunrise.replace(tzinfo=None)
+
+    friday_sunset, _ = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], friday_dt)
+    friday_sunset = friday_sunset.replace(tzinfo=None)
+
+    _, saturday_sunrise = get_sunset_sunrise_times(device_settings['lat'], device_settings['lon'], saturday_dt)
+    saturday_sunrise = saturday_sunrise.replace(tzinfo=None)
 
     # Every schedule starts on a Monday and ends on a Sunday, total Witty Pi schedule to cover this full week schedule
     # Overlapping schedule starts 10 minutes earlier to the nearest schedule and ends 10 minutes later to the last schedule
