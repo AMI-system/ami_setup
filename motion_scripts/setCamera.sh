69db359d2c65150af54a851e575413d7fcbced98
albags <albseg@ceh.ac.uk>
2024-05-28 14:20:32 +0100

    Update setCamera.sh

--- motion_scripts/setCamera.sh
+++ motion_scripts/setCamera.sh
@@ -2,10 +2,10 @@ v4l2-ctl -d /dev/video0 -c brightness=100
 v4l2-ctl -d /dev/video0 -c focus_auto=0
 v4l2-ctl -d /dev/video0 -c focus_absolute=50
 v4l2-ctl -d /dev/video0 -c exposure_auto=1
-v4l2-ctl -d /dev/video0 -c exposure_absolute=40
+v4l2-ctl -d /dev/video0 -c exposure_absolute=80
 v4l2-ctl -d /dev/video0 -c zoom_absolute=100
 v4l2-ctl -d /dev/video0 -c white_balance_temperature_auto=0
-v4l2-ctl -d /dev/video0 -c white_balance_temperature=4000
+v4l2-ctl -d /dev/video0 -c white_balance_temperature=7000
 v4l2-ctl -d /dev/video0 -c sharpness=25
 v4l2-ctl -d /dev/video0 -c contrast=20
 
