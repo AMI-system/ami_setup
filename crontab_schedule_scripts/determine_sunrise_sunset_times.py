478b7c6617dbf1e1bcfc484012b3701f38c0bf70
albags <albseg@ceh.ac.uk>
2024-05-29 14:33:38 +0100

    Update determine times file
    Sunset time should be based on "today" date and surinse time on "tomorrow" date.

--- crontab_schedule_scripts/determine_sunrise_sunset_times.py
+++ crontab_schedule_scripts/determine_sunrise_sunset_times.py
@@ -5,21 +5,24 @@ import json
 from pathlib import Path
 from datetime import datetime, timedelta
 
-from utils.shared_functions import read_json_config, get_sunset_sunrise_times, get_current_time
+from utils.shared_functions import read_json_config, get_sunset_sunrise_times
 
+CONFIG_PATH = Path('/home/pi/config.json')
 
 if __name__ == "__main__":
     # Read the config file
-    config_path = Path('/home/pi/config.json')
+    config_path = CONFIG_PATH
 
     # Read the JSON configuration file
     config = read_json_config(config_path)
 
     # Find today's and tomorrow's dates
     today = datetime.now()
+    tomorrow = today + timedelta(1)
 
     # Get civil sunset and sunrise for today and tomorrow
-    sunset, sunrise = get_sunset_sunrise_times(config['device_settings']['lat'], config['device_settings']['lon'], today)
+    sunset, _ = get_sunset_sunrise_times(config['device_settings']['lat'], config['device_settings']['lon'], today)
+    _, sunrise = get_sunset_sunrise_times(config['device_settings']['lat'], config['device_settings']['lon'], today)
 
     # print("Todays's sunset:", sunset)
     # print("Tomorrow's sunrise", sunrise)
@@ -29,5 +32,5 @@ if __name__ == "__main__":
     config["sunrise_sunset_times"]["sunrise_time"] = sunrise.strftime('%H:%M:%S%z')
 
     # Save the updated config.json
-    with open('/home/pi/config.json', 'w') as file:
+    with open(CONFIG_PATH, 'w') as file:
         json.dump(config, file, indent=4)
