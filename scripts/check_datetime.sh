447e280ecf124e1b179821318c24a52d68f7efd7
Grace Skinner <graski@ceh.ac.uk>
2024-09-23 13:21:20 +0100

    resetting time working when connects

--- scripts/check_datetime.sh
+++ scripts/check_datetime.sh
@@ -17,16 +17,17 @@ if [[ "$system_time" -lt "$cutoff_datetime" ]]; then
 	
 	# Retrieve time from cellular
 	current_datetime=$(sudo python3 /home/pi/scripts/retrieve_cellular_time.py)
-	#current_datetime="2024-09-18 16:38:29"
-	echo "$current_datetime"
-	echo "I have just printed the current datetime"
+	if [ $? -eq 0 ]; then
+		echo "Python script succeeded"
+		echo "$current_datetime"
+		echo "I have just printed the current datetime"
+		
+		# Set datetime from cellular
+		sudo date -s "${current_datetime}"
+		echo "I have just set the date from cellular"
 	
-	# Set datetime from cellular
-	sudo date -s "${current_datetime}"
-	echo "I have just set the date from cellular"
-	
-	# What about setting rtc time after this??
-    expect <<EOF
+		# What about setting rtc time after this??
+		expect <<EOF
 spawn sudo /home/pi/wittypi/wittyPi.sh
 expect "What do you want to do? (1~13)"
 send "1\r"
@@ -34,8 +35,23 @@ expect "What do you want to do? (1~13)"
 send "13\r"
 expect eof
 EOF
+	else
+		echo "Python script failed"
+		exit 1
+	fi
+	
 else
 	echo "System datetime ($system_time) is after cutoff datetime ($cutoff_datetime)." 
 	echo "No change required."
 fi
+
+
+
+	
+
+
+
+	
+
+
 
--- scripts/retrieve_cellular_time.py
+++ scripts/retrieve_cellular_time.py
@@ -1,112 +1,48 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-#from utils.shared_functions import get_cellular_time
-#from datetime import datetime
-
 from datetime import datetime, timedelta
 from periphery import I2C
 import notecard
 from notecard import card
 import pytz
-
-# from timezonefinder import TimezoneFinder
-# import json
-# import subprocess
-# import re
-
-# Use python-periphery on a Linux desktop or RPi
-#from time import sleep
+from time import sleep
 
 def main():
-	try:
-		i2c_path = "/dev/i2c-1"
-		port = I2C(i2c_path)
-		nCard = notecard.OpenI2C(port, 0, 0)
-		timeout = 60
-		got_time = False
-		
-		for iteration in range(timeout):
-			response = card.time(nCard)
-			time = response["time"]
-			zone = response["zone"].split(",")[1]
-			if zone != "Unknown":
-				got_time = True
-				utc_datetime = datetime.utcfromtimestamp(time)
-				local_timezone = pytz.timezone(zone)
-				local_datetime = utc_datetime.astimezone(local_timezone)
-				local_datetime = local_datetime.strftime('%Y-%m-%d %H:%M:%S')
-				break
-				sleep(1)
-			if not got_time:
-				raise Exception("Failed to set local time of day via card time after 60 attempts")
-			
-		print(local_datetime)
-				
+	i2c_path = "/dev/i2c-1"
+	port = I2C(i2c_path)
+	nCard = notecard.OpenI2C(port, 0, 0)
+	timeout = 300
+	got_time = False
 	
-	except Exception as e:
-		print(f"Error: {e}")
-		exit(1)
-		
-			
-
-# def main():
-    # try:
-        # # current_datetime = get_cellular_time()
-        # # print(current_datetime.strftime('%Y-%m-%d %H:%M:%S'))
+	for iteration in range(timeout):
+		try:
+			#print(f"Attempt {iteration+1}:")
+			response = card.time(nCard) # Get time from modem if finished booting and established cellular connectivity
+			#print(response)
+			#print(x)
+			if "time" in response and "err" not in response: # Check if modem returned a time
+				time = response["time"]
+				#print(time)
+				if "zone" in response and len(response["zone"].split(",")) > 0: # Check if modem returned a timezone
+					zone = response["zone"].split(",")[1] # Extract timezone
+					if zone != "Unknown": # check timezone is valid
+						got_time = True
+						utc_datetime = datetime.utcfromtimestamp(time)
+						local_timezone = pytz.timezone(zone)
+						local_datetime = utc_datetime.astimezone(local_timezone)
+						local_datetime = local_datetime.strftime('%Y-%m-%d %H:%M:%S')
+						print(local_datetime)
+						break
+			else:
+				raise Exception("Retrieving time did not work")
+		except Exception as e:
+			#print(f"Attempt {iteration+1} failed with error: {e}. Retrying in 1 second...")
+			sleep(1)
 	
-        # # current_datetime = get_cellular_time().strftime('%Y-%m-%d %H:%M:%S')
-		
-        # #current_datetime_1 = "2024-09-18 17:02:29"
-        # #current_datetime_1 = f"{current_datetime_1[:13]}:00:00"
-        # #print(f"1: {current_datetime_1} (Type: {type(current_datetime_1)})")
-        # #current_datetime_2 = get_cellular_time().strftime('%Y-%m-%d %H:%M:%S')
-        # #current_datetime_2 = f"{current_datetime_2[:13]}:00:00"
-        # #print(f"2: {current_datetime_2} (Type: {type(current_datetime_2)})")
-        
-        # # if current_datetime_1 == current_datetime_2:
-            # # print("They match")
-        # # else:
-            # # print("They don't match")
-		
-        # #print(current_datetime_1)
-        # #print(f"Type: {type(current_datetime)}")
-        
-        # #print(f"{current_datetime_2}")
-        
-        
-        # # Configure I2C (connection between Notecard and RPi)
-        # i2c_path = "/dev/i2c-1"
-        # port = I2C(i2c_path)
-        # # Connect to Notecard via I2C
-        # nCard = notecard.OpenI2C(port, 0, 0)
-		
-        # #print("Wait for Notecard to acquire time of day")
-        # timeout = 60
-        # got_time = False
-		
-        # for iteration in range(timeout):
-            # response = card.time(nCard)
-            # time = response["time"]
-            # #print(time)
-            # zone = response["zone"].split(",")[1]
-            # if zone != "Unknown":
-                # got_time = True
-                # utc_datetime = datetime.utcfromtimestamp(time)
-                # utc_datetime = utc_datetime.replace(tzinfo=pytz.utc)
-                # local_timezone = pytz.timezone(zone)
-                # local_datetime = utc_datetime.astimezone(local_timezone)
-                # local_datetime = local_datetime.strftime('%Y-%m-%d %H:%M:%S')
-                # break
-                # sleep(1)
-            # if not got_time:
-                # raise Exception("Failed to set local time of day via card time after 60 attempts")
-            
-            # print(local_datetime)
-        
-    # except Exception as e:
-		# print(f"Error: {e}")
-		# exit(1)
+	else: # If the for loop runs without success (i.e. break never initiated)
+		print("All attempts failed")
+		raise Exception(f"Failed to set local time of day via card time after {iteration+1} attempts")
 
 if __name__ == "__main__":
 	main()
